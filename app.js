6c1a2a5abe227a2fbfe28a60bba06a0a8b09b705
Sam Bush <samb@stat8.net>
2015-06-07 19:22:26 -0400

    test

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 
 $(document).ready(function(){
 	
-	
-	 
 		
-		
-
-
 	
+			
+			var listHtml = "";
+			
+			var url = "http://www.zippod.net/RYP/jsonCurrent.php"
+			$.post(url, function(response){
+			var json = $.parseJSON(response);
+			
+			
+			$.each(json, function(key, value){  
+				
+				
+				listHtml += "<li><a href='" + value.link +  "'><img class='ui-circle ui-mini ui-padding' src='"+ value.image +"'><h2>" + value.name +  "</h2><p><strong>"+ value.title + "</strong></p><p><strong>" + value.date +"</strong></p></li>";
+				});//end each
+			
+			
+		$("#history").html(listHtml); 
+		$('ul').listview('refresh');
+
+			});//end post podlist
+			
+			
 	});//end document ready
